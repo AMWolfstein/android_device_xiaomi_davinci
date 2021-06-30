9c4d9b3ca0aa9baa7e77e39b722de18339066045
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-06-30 11:47:34 +0200

    davinci: Use QTI USB init configs from USB HAL repo
    Change-Id: Ic9dc848773e996c60de0e9f273a7e9eaec178b3b

--- device.mk
+++ device.mk
@@ -387,7 +387,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.qcom.power.rc \
     init.qcom.rc \
-    init.qcom.usb.rc \
     init.recovery.qcom.rc \
     init.xiaomi.rc \
     ueventd.qcom.rc
@@ -443,6 +442,13 @@ PRODUCT_PACKAGES += \
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.dual_role_usb
+
+PRODUCT_PACKAGES += \
+    init.qcom.usb.rc \
+    init.qcom.usb.sh
+
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/qcom/opensource/usb/etc
 
 # Vendor service manager
 PRODUCT_PACKAGES += \
