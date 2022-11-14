ddc57d7a800bc62f1654d6ab4e1d85f79992e48b
AMWolfstein <76236780+AMWolfstein@users.noreply.github.com>
2022-11-14 10:38:32 +0200

    davinci: Build Gcam go v2

--- device.mk
+++ device.mk
@@ -109,6 +109,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
     libdng_sdk.vendor \
+    GCamGOPrebuilt-V2 \
     libpiex_shim
 
 PRODUCT_COPY_FILES += \
